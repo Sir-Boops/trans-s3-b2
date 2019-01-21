0f6cc19a387d484a1860df6856376c2a7a8228d6
Sir_Boops <admin@boops.me>
2019-01-21 20:44:45 +0000

    If nothing is done return 500

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 
   BucketID, API_URL, AUTH, DL_URL := get_keys(os.Args[1], os.Args[2])
 
+  did_something := false
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     bodyBytes, _ := ioutil.ReadAll(r.Body)
     path := r.URL.String()
@@ -24,12 +26,24 @@ func main() {
       UAUTH, UURL := upload_url(BucketID, API_URL, AUTH)
       upload_file(UAUTH, UURL, path, content_type, bodyBytes, os.Args[3])
       fmt.Println("Uploaded: " + path)
+      did_something = true
     }
 
     // Delete a file
     if r.Method == "DELETE" {
       rm_file(AUTH, DL_URL, path)
       fmt.Println("Deleted: " + path)
+      did_something = true
+    }
+
+    // Just ignore HEADs
+    if r.Method == "HEAD" {
+      did_something = true
+    }
+
+    // Make sure something was done
+    if !did_something {
+      w.WriteHeader(http.StatusInternalServerError)
     }
 
   })
