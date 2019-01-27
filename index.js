59eaa40da660baa5a65b1cfeb4f471a10ac885d4
Sir_Boops <admin@boops.me>
2019-01-27 07:28:50 -0700

    Try to BS a response

--- index.js
+++ index.js
@@ -90,8 +90,12 @@ db.run('CREATE TABLE hashes (id TEXT, hash TEXT, part INT)', function(err){
 				// Finish large file upload
 				b2_finish_large_file.b2_finish_large_file(auth, req.query.uploadId, db, function(status){
 					console.log('Fished multi part upload: ' + req.query.uploadId)
+					const ans = '<?xml version="1.0" encoding="UTF-8"?>' +
+					'<CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">' +
+					'<Location>https://' + process.argv[4] + '/file/' + req.path + '</Location>' +
+					'</CompleteMultipartUploadResult>'
 					res.status(status)
-					res.send()
+					res.send(ans)
 				})
 			}
 		})
